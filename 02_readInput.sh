78ec612ed58cebc55c18558fa6f91cf491853903
bklobb <battula.balakrishna@lobb.in>
2025-04-07 16:26:55 +0530

    Added readInput

---
+++ 02_readInput.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "Enter your name please"
+
+read name
+
+echo "Your name is: $name"
+
+read -p "Enter your age: " age
+
+echo "Your age is: $age"
+
+
+read -p "Username: " USERNAME
+
+read -sp "Password: " PASSWORD
+
+echo
+
+echo "Username is $USERNAME and Password is $PASSWORD"
